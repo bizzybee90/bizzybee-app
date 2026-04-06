a2cb81b95c19ea953aff4cc916ba68bef68e1a93
Michael Carbon <michael@maccleaning.uk>
2026-04-06 11:43:45 +0100

    Restore home supabase client import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { supabase } from '@/integrations/supabase/client';
 import { Skeleton } from '@/components/ui/skeleton';
 import { ThreeColumnLayout } from '@/components/layout/ThreeColumnLayout';
 import { Sidebar } from '@/components/sidebar/Sidebar';
